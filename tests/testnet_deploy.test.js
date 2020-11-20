0e64e40e3604a3aa52f3fc4c7657986b3ad140b8
Noah Thorp <noah@comakery.com>
2020-11-20 00:36:44 -0800

    Verify global and local state after contract deployment.

--- tests/testnet_deploy.test.js
+++ tests/testnet_deploy.test.js
@@ -12,7 +12,27 @@ const token = {
 
 test('testnet deploy', async () => {
     let algodClient = new algosdk.Algodv2(token, baseServer, port);
-
     let info = await util.deploySecurityToken(algodClient, recoveredAccount)
-    console.log(info)
+    let localState = await util.readLocalState(algodClient, recoveredAccount, info.appId)
+    expect(localState.sort()).toEqual(
+        [
+            { key: 'contract admin', value: { bytes: '', type: 2, uint: 1 } },
+            { key: 'transfer admin', value: { bytes: '', type: 2, uint: 1 } },
+            { key: 'balance', value: { bytes: '', type: 2, uint: 0 } }
+        ].sort()
+    )
+    let globalState = await util.readGlobalState(algodClient, recoveredAccount, info.appId)
+    expect(globalState.sort()).toEqual(
+        [
+            { key: 'paused', value: { bytes: '', type: 2, uint: 0 } },
+            {
+                key: 'total supply',
+                value: { bytes: '', type: 2, uint: 80000000000000000 }
+            },
+            {
+                key: 'reserve',
+                value: { bytes: '', type: 2, uint: 80000000000000000 }
+            }
+        ].sort()
+    )
 })
