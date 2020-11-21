f7eb26680b7311973416395ce88a3c83e21a4d3d
Noah Thorp <noah@comakery.com>
2020-11-21 00:17:55 -0800

    Test minting tokens.

--- tests/testnet_deploy.test.js
+++ tests/testnet_deploy.test.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const shell = require('shelljs')
 const util = require('../lib/algoUtil')
+const { TextEncoder } = require('util')
 const algosdk = require('algosdk')
 
 const server = "http://127.0.0.1"
@@ -30,6 +31,37 @@ test('test initial deployment state', async () => {
     expect(globalState['total supply']['uint']).toEqual(80000000000000000) // TODO: rename to total
     expect(globalState['decimals']['uint']).toEqual(8)
     expect(globalState['unitname']['bytes']).toEqual("ABCTEST")
+
+    //mint
+    let params = await clientV2.getTransactionParams().do()
+    params.fee = 1000
+    params.flatFee = true
+
+    let enc = new TextEncoder()
+    appArgs = []
+    appArgs.push(enc.encode("mint"))
+    appArgs.push(util.bigIntToUint8Array('27'))
+
+    let txn = algosdk.makeApplicationNoOpTxn(recoveredAccount.addr, params, info.appId, appArgs, [recoveredAccount.addr])
+    let signedTxn = algosdk.signTransaction(txn, recoveredAccount.sk)
+    let sendTx = await clientV2.sendRawTransaction(signedTxn.blob).do()
+    console.log("Transaction : ", sendTx)
+    await util.waitForConfirmation(clientV2, sendTx.txId)
+    let transactionResponse = await clientV2.pendingTransactionInformation(sendTx.txId).do()
+    console.log(`transaction response: ${transactionResponse}`)
+
+    localState = await util.readLocalState(clientV2, recoveredAccount, info.appId)
+    expect(localState["balance"]["uint"]).toEqual(27)
+    //TODO: add vettingsAdmin
+
+    globalState = await util.readGlobalState(clientV2, recoveredAccount, info.appId)
+    expect(globalState['total supply']['uint']).toEqual(
+        80000000000000000)
+    expect(globalState['reserve']['uint']).toEqual(
+        79999999999999973)
+// TODO: rename to total
+
+    //transfer
 })
 
 //TODO: verify only approved account can upgrade
