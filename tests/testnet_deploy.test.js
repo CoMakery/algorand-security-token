a81de9019f24987e3dab8908e8709529449fad79
Noah Thorp <noah@comakery.com>
2020-11-22 14:23:17 -0800

    Parameter arguments for contract deploy lib and tests

--- tests/testnet_deploy.test.js
+++ tests/testnet_deploy.test.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 const shell = require('shelljs')
 const util = require('../lib/algoUtil')
-const {EncodeUint, EncodeBytes} = util
 const algosdk = require('algosdk')
 
 const server = "http://127.0.0.1"
@@ -21,14 +20,31 @@ test('test initial deployment state', async () => {
     let info = await util.deploySecurityToken(clientV2, adminAccount)
     let localState = await util.readLocalState(clientV2, adminAccount, info.appId)
     expect(localState["balance"]["uint"]).toEqual(0)
-    expect(localState["contract admin"]["uint"]).toEqual(1) // TODO: rename to contractAdmin
-    expect(localState["transfer admin"]["uint"]).toEqual(1) // TODO: rename to transferAdmin
+    expect(localState["contract admin"]["uint"].toString()).toEqual('1') // TODO: rename to contractAdmin
+    expect(localState["transfer admin"]["uint"].toString()).toEqual('1') // TODO: rename to transferAdmin
     //TODO: add vettingsAdmin
 
     let globalState = await util.readGlobalState(clientV2, adminAccount, info.appId)
-    expect(globalState['paused']['uint']).toEqual(0)
-    expect(globalState['reserve']['uint']).toEqual(80000000000000000)
-    expect(globalState['total supply']['uint']).toEqual(80000000000000000) // TODO: rename to total
-    expect(globalState['decimals']['uint']).toEqual(8)
+    expect(globalState['paused']['uint'].toString()).toEqual('0')
+    expect(globalState['reserve']['uint'].toString()).toEqual('80000000000000000')
+    expect(globalState['total supply']['uint'].toString()).toEqual('80000000000000000') // TODO: rename to total
+    expect(globalState['decimals']['uint'].toString()).toEqual('8')
     expect(globalState['unitname']['bytes']).toEqual("ABCTEST")
+})
+
+test('test initial deployment args', async () => {
+    //TODO: can pass in total, decimals, unitname
+    let info = await util.deploySecurityToken(clientV2, adminAccount, 1234567, 7, "XYZ2")
+    let localState = await util.readLocalState(clientV2, adminAccount, info.appId)
+    expect(localState["balance"]["uint"]).toEqual(0)
+    expect(localState["contract admin"]["uint"].toString()).toEqual('1') // TODO: rename to contractAdmin
+    expect(localState["transfer admin"]["uint"].toString()).toEqual('1') // TODO: rename to transferAdmin
+    //TODO: add vettingsAdmin
+
+    let globalState = await util.readGlobalState(clientV2, adminAccount, info.appId)
+    expect(globalState['paused']['uint'].toString()).toEqual('0')
+    expect(globalState['reserve']['uint'].toString()).toEqual('1234567')
+    expect(globalState['total supply']['uint'].toString()).toEqual('1234567') // TODO: rename to total
+    expect(globalState['decimals']['uint'].toString()).toEqual('7')
+    expect(globalState['unitname']['bytes']).toEqual("XYZ2")
 })
