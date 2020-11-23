f2c42ca62cfafa227891f6ed9b420831ed0e5f60
Noah Thorp <noah@comakery.com>
2020-11-22 21:04:29 -0800

    more bin scripts

---
+++ bin/read_global_state.sh
@@ -0,0 +1,3 @@
+# usage
+# bin/read_global_state.sh appId
+goal app read --global --app-id $1 -d devnet/Primary
---
+++ bin/read_local_state.sh
@@ -0,0 +1,3 @@
+# usage
+# bin/read_local_state.sh appId accountAddress
+goal app read --local --app-id $1 --from $2 -d devnet/Primary
