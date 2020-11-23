9be44ae7bf09e2746515d8a8215401953d4f1d4f
Noah Thorp <noah@comakery.com>
2020-11-22 16:05:50 -0800

    Add datadir to mint script

--- bin/mint.sh
+++ bin/mint.sh
@@ -1,3 +1,3 @@
 # usage:
 # bin/mint.sh appId fromToAddress amount
-goal app call --app-id $1 --from $2 --app-account $2 --app-arg 'str:mint' --app-arg "int:${3}"
+goal app call --app-id $1 --from $2 --app-account $2 --app-arg 'str:mint' --app-arg "int:${3}" -d devnet/Primary
