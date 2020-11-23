e1212eaa650a0838252f1264abac2df328583f3a
Noah Thorp <noah@comakery.com>
2020-11-23 07:46:12 -0800

    Contract admin can revoke original contract admin's role

--- tests/contract_admin.test.js
+++ tests/contract_admin.test.js
@@ -46,7 +46,46 @@ test('contract admin can grant contract admin role - which allows granting the c
     await util.optInApp(clientV2, accounts[2], appId)
     await util.appCall(clientV2, receiverAccount, appId, appArgs, [accounts[2].addr])
 
-    // check receiver did not get tokens
     localState = await util.readLocalState(clientV2, accounts[2], appId)
     expect(localState["contract admin"]["ui"]).toEqual(1)
+})
+
+test('contract admin can revoke the original contract admin role', async () => {
+    // grant contract admin
+    appArgs = [
+        EncodeBytes("set admin"),
+        EncodeBytes("contract admin"),
+        EncodeUint('1')
+    ]
+
+    await util.appCall(clientV2, adminAccount, appId, appArgs, [receiverAccount.addr])
+
+    localState = await util.readLocalState(clientV2, receiverAccount, appId)
+    expect(localState["contract admin"]["ui"]).toEqual(1)
+
+    // new contract admin grants contract admin to another new admin
+    appArgs = [
+        EncodeBytes("set admin"),
+        EncodeBytes("contract admin"),
+        EncodeUint('0')
+    ]
+
+    await util.appCall(clientV2, receiverAccount, appId, appArgs, [adminAccount.addr])
+    localState = await util.readLocalState(clientV2, adminAccount, appId)
+    expect(localState["contract admin"]["ui"]).toEqual(undefined)
+
+    // original admin tries to revoke 2nd admins rights but no longer has that privilege
+    appArgs = [
+        EncodeBytes("set admin"),
+        EncodeBytes("contract admin"),
+        EncodeUint('0')
+    ]
+
+    let revokedAdminLostAbilityToGrantAdminRole = false
+    try {
+        await util.appCall(clientV2, adminAccount, appId, appArgs, [receiverAccount.addr])
+    } catch (e) {
+        revokedAdminLostAbilityToGrantAdminRole = true
+    }
+    expect(revokedAdminLostAbilityToGrantAdminRole).toEqual(true)
 })
