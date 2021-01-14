4475be34635124d795b89654af5d216133dd18c3
Alex C <no-reply@wwwwww.pw>
2021-01-14 19:46:15 +0300

    Fix development node start
    - Symlink `algod.net` to `algod-listen.net`

--- bin/start-devnet.sh
+++ bin/start-devnet.sh
@@ -11,6 +11,7 @@ goal kmd stop -d $NETWORK_PRIMARY_KEY
 rm -rf $NETWORK_DIR
 goal network create -r $NETWORK_DIR -n private -t $CONFIG_DIR
 cp ./config/config.json devnet/Primary #to enable TEAL compilation, copy in a config with "EnableDeveloperAPI": true
+ln -s $NETWORK_PRIMARY_KEY/algod.net $NETWORK_PRIMARY_KEY/algod-listen.net
 goal network start -r $NETWORK_DIR -k $DEVNET_KMD -d $NETWORK_PRIMARY_KEY
 goal kmd start -d $NETWORK_PRIMARY_KEY
 echo $NETWORK_DIR
