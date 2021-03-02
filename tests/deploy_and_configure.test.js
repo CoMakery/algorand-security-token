1d5079182f8eb84a6da9422a2e373bc0dff7422b
Noah Thorp <noah@comakery.com>
2021-03-01 23:17:39 -0800

    Factor out deploy configuration from spec

--- tests/deploy_and_configure.test.js
+++ tests/deploy_and_configure.test.js
@@ -14,15 +14,20 @@ beforeAll(async () => {
     reserveContractAdminAccount = accounts[0]
     tempLaunchAccount = accounts[1]
     manualAdminAccount = accounts[2]
-
     hotWalletAccount = accounts[3]
+
     token = await shell.cat(`devnet/Primary/algod.token`).stdout
     client = new algosdk.Algodv2(token, server, port)
+
     config = {
-        tempLaunchAccountAddress: tempLaunchAccount.addr,
+        tempLaunchAccount: tempLaunchAccount,
         manualAdminAccountAddress: manualAdminAccount.addr,
         hotWalletAccountAddress: hotWalletAccount.addr,
-        transferRules: [{from: 2, to: 3, after: 1}, {from: 3, to: 4, after: 1}]
+        transferRules: [
+            {from: 2, to: 3, after: 1},
+            {from: 3, to: 4, after: 1}
+            ],
+        hotWalletBalance: 1000
     }
 })
 
@@ -39,28 +44,14 @@ async function checkOptedInAndHasAlgos(_client, _addr, appId) {
     )
 }
 
-test('test deployment configuration script', async () => {
-    let info = await util.deploySecurityToken(client, reserveContractAdminAccount)
-    let appId = info.appId
-    console.log(info)
-
-    // all initial accounts opt in before the tempLaunchAccount configures everything
-    await Promise.all([
-        util.optInApp(client, tempLaunchAccount, appId),
-        util.optInApp(client, manualAdminAccount, appId),
-        util.optInApp(client, hotWalletAccount, appId)
-    ])
-
-    await checkOptedInAndHasAlgos(client, config.tempLaunchAccountAddress, appId)
+async function checkAllAccountsReady(client, appId, config) {
+    // check all accounts to be configured have opted in to the application and have algos for gas
+    await checkOptedInAndHasAlgos(client, config.tempLaunchAccount.addr, appId)
     await checkOptedInAndHasAlgos(client, config.manualAdminAccountAddress, appId)
     await checkOptedInAndHasAlgos(client, config.hotWalletAccountAddress, appId)
+}
 
-    // check all accounts to be configured have opted in to the application and have algos for gas
-
-    // setup tempLaunchAccount that will configure everything by script and then have role removed
-    // tempLaunchAccount has no token balance and no transfer group
-    await util.grantRoles(client, appId, reserveContractAdminAccount, config.tempLaunchAccountAddress, 15)
-
+async function configureContract(client, appId, config) {
     // set the transfer rules
 
     await Promise.all(config.transferRules.map(rule => {
@@ -75,18 +66,39 @@ test('test deployment configuration script', async () => {
     )
 
     // manualAdmin account: transferRules, walletAdmin, admin transfer group
-    await util.grantRoles(client, appId, tempLaunchAccount, config.manualAdminAccountAddress, 3)
-    await util.setAddressPermissions(client, appId, tempLaunchAccount, config.manualAdminAccountAddress, 0, 0, 0, 2)
+    await util.grantRoles(client, appId, config.tempLaunchAccount, config.manualAdminAccountAddress, 3)
+    await util.setAddressPermissions(client, appId, config.tempLaunchAccount, config.manualAdminAccountAddress, 0, 0, 0, 2)
 
     // hotWallet: walletAdmin, admin transfer group
-    await util.grantRoles(client, appId, tempLaunchAccount, config.hotWalletAccountAddress, 1)
-    await util.setAddressPermissions(client, appId, tempLaunchAccount, config.hotWalletAccountAddress, 0, 0, 0, 2)
+    await util.grantRoles(client, appId, config.tempLaunchAccount, config.hotWalletAccountAddress, 1)
+    await util.setAddressPermissions(client, appId, config.tempLaunchAccount, config.hotWalletAccountAddress, 0, 0, 0, 2)
 
     // the reserve admin mints initial tokens needed for hot wallet distribution into the hot wallet
-    await util.mint(client, appId, tempLaunchAccount, config.hotWalletAccountAddress, 1000)
+    await util.mint(client, appId, config.tempLaunchAccount, config.hotWalletAccountAddress, config.hotWalletBalance)
+}
+
+test('test deployment configuration script', async () => {
+    let info = await util.deploySecurityToken(client, reserveContractAdminAccount)
+    let appId = info.appId
+    console.log(info)
+
+    // all initial accounts opt in before the tempLaunchAccount configures everything
+    await Promise.all([
+        util.optInApp(client, tempLaunchAccount, appId),
+        util.optInApp(client, manualAdminAccount, appId),
+        util.optInApp(client, hotWalletAccount, appId)
+    ])
+
+    checkAllAccountsReady(client, appId, config)
+
+    // setup tempLaunchAccount that will configure everything by script and then have role removed
+    // tempLaunchAccount has no token balance and no transfer group
+    await util.grantRoles(client, appId, reserveContractAdminAccount, config.tempLaunchAccount.addr, 15)
+
+    configureContract(client, appId, config)
 
     // remove launch account, this may need to be done manually by a multi-sig contract admin account
-    await util.grantRoles(client, appId, reserveContractAdminAccount, config.tempLaunchAccountAddress, 0)
+    // await util.grantRoles(client, appId, reserveContractAdminAccount, config.tempLaunchAccountAddress, 0)
 
     // assert validation, for use after actual deployment
 })
