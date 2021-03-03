d470344118a8cbcd492b762f4d1b1ce729a13ca8
Noah Thorp <noah@comakery.com>
2021-03-02 19:20:30 -0800

    Improved app state checking parser

--- deploy/5_check_final_state.js
+++ deploy/5_check_final_state.js
@@ -3,15 +3,58 @@
 const config = require('./deploy.config.js')
 const util = require('../lib/algoUtil')
 
-// this script outputs three transactions to be used by OREID
-// They should be signed by the reserve / contract admin account
-// transaction to
-//
-;(async() => {
-    let application = await config.client.getApplicationByID(config.appId).do()
+async function checkAccountConfig(name, address, _config = config) {
+    let accountInfo = await _config.client.accountInformation(address).do()
+    let values = {
+        name: name,
+        address: address,
+        optedIn: accountInfo['apps-local-state'].some(y => y['id'] == _config.appId),
+        algoBalance: accountInfo.amount
+    }
+    values.ready = values.algoBalance > 3000 && values.optedIn == true
+
+    if(values.optedIn) {
+        let localState = util.decodeState(accountInfo['apps-local-state'].find(y => y['id'] == _config.appId)['key-value'])
+        values.roles = localState.roles.uint
+        values.transferGroup = localState.uint
+        values.balance = localState.balance.uint
+    } else {
+        values.localState = null
+    }
+
+    return values
+}
+
+async function getGlobalAppState(client, appId) {
+    let application = await client.getApplicationByID(appId).do()
     let globalState = application['params']['global-state']
-    console.log(util.decodeState(globalState))
-    console.log(util.decodeState("MORE CHECKS REMAINING - NOT FULLY IMPLEMENTED YET"))
+    let dState = util.decodeState(globalState)
+    return {
+        symbol: dState.symbol.bytes,
+        totalSupply: dState.totalSupply.uint,
+        cap: dState.cap.uint,
+        decimals: dState.decimals.uint,
+        name: dState.name.bytes,
+        paused: dState.paused.uint,
+        reserve: dState.reserve.uint
+    }
+}
+
+
+;(async() => {
+    let tempLaunchAccountInfo = await checkAccountConfig("Temp Launch Account", config.tempLaunchAccount.addr)
+    let checks = await Promise.all([
+        getGlobalAppState(config.client, config.appId),
+        checkAccountConfig("Contract Admin Reserve Account", config.contractAdminReserveAccountAddress),
+        tempLaunchAccountInfo,
+        checkAccountConfig("Manual Admin Account Address", config.manualAdminAccountAddress),
+        checkAccountConfig("Hot Wallet Account Address", config.hotWalletAccountAddress)
+    ])
+
+    console.log(checks)
+    if(tempLaunchAccountInfo.roles > 0) {
+        console.log(`\nERROR!!! TEMP LAUNCH ACCOUNT STILL AN ADMIN WITH ROLE ${tempLaunchAccountInfo.roles}!!!\n`)
+    }
 })().catch(e => {
     console.log(e)
 })
